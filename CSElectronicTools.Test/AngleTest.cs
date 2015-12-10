208ef3498b45ebc9f1649e5f3fbf3e2c06a3cca9
Lassi <csharplassi@posteo.de>
2015-12-10 08:52:38 +0100

    Angle primitiv hinzugefügt

---
+++ CSElectronicTools.Test/AngleTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CSElectronicTools.Test
+{
+    [TestFixture()]
+    public class AngleTest
+    {
+        [Test()]
+        public void CreateTest()
+        {
+            Angle ang = Math.PI;
+            Assert.AreEqual(ang.Value, Math.PI, 0.1);
+
+            double angvalue = ang;
+            Assert.AreEqual(angvalue, Math.PI, 0.1);
+
+
+        }
+    }
+}
+
---
+++ CSElectronicTools/Angle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSElectronicTools
+{
+    public class Angle
+    {
+        public double Value
+        {
+            get;
+            set;
+        }
+
+        public Angle()
+        {
+        }
+
+        public Angle(double value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator Angle(double value)
+        {
+            return new Angle(value);
+        }
+
+        public static implicit operator double(Angle value)
+        {
+            return value.Value;
+        }
+
+    }
+}
+
